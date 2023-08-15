cb42cad3e471ffc7c29fced199c9c5328c9f17ca
rodrigo8ueno <rm98385@fiap.com.br>
2023-08-14 21:45:37 -0300

    Aula de um programa de estoque

--- Dictionary.py
+++ Dictionary.py
@@ -1,15 +1,15 @@
-#Aula sobre dicionário
-tabela = {"alface":0.45, "batata":1.2, "tomate": 2.3, "feijão": 1.5}
-while True:
-    produto = input("Digite o nome do produto ou fim para terminar:")
-    if produto =="fim":
-        break
-    if produto in tabela:
-        print(f"Preço: R$ {tabela[produto]:.2f}")
-    else:
-        print("Produto não encontrado.")
-
-print(tabela)
+# Aula sobre dicionário
+# tabela = {"alface":0.45, "batata":1.2, "tomate": 2.3, "feijão": 1.5}
+# while True:
+#     produto = input("Digite o nome do produto ou fim para terminar:")
+#     if produto =="fim":
+#         break
+#     if produto in tabela:
+#         print(f"Preço: R$ {tabela[produto]:.2f}")
+#     else:
+#         print("Produto não encontrado.")
+#
+# print(tabela)
 
 
 # carros = {"Jeep Renegade":['R$90.000,00', 'Ano 2018'], "Jeep Compass":'R$150.000,00', "Troller": 'R$200.000,00'}
@@ -34,3 +34,25 @@ print(tabela)
 #
 # print(carros.keys())
 # print(carros.values())
+
+#Aula de Dicionário com Lista embutida
+
+estoque = {"tomate": [1000, 2.3], "alface": [500, 0.45], "batata": [2001, 1.2], "feijão": [100, 1.5]}
+
+venda = [["tomate", 5], ["batata", 10], ["alface", 5]]
+
+total = 0
+
+for operacao in venda:
+  produto, quantidade = operacao
+  preco = estoque[produto][1]
+  custo = preco * quantidade
+  print(f"{produto}: {quantidade} x {preco} = {custo}")
+  estoque[produto][0] -= quantidade
+  total += custo
+
+  print(f"Custo total: {total}")
+  for chave, dados in estoque.items():
+    print("Descrição: ", chave)
+    print("Quantidade: ", dados[0])
+    print(f"Preço: R$ {dados[1]}")
